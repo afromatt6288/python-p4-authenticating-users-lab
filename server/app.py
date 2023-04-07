bc878428df6195c71e83c2fcbf1c9e7b1cd4fd9c
Matthew Clark <afromatt6288@hotmail.com>
2023-04-07 13:05:25 -0600

    Lab Complete

--- server/app.py
+++ server/app.py
@@ -19,22 +19,17 @@ db.init_app(app)
 api = Api(app)
 
 class ClearSession(Resource):
-
-    def delete(self):
-    
+    def delete(self):    
         session['page_views'] = None
         session['user_id'] = None
-
         return {}, 204
 
-class IndexArticle(Resource):
-    
+class IndexArticle(Resource):    
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return articles, 200
 
 class ShowArticle(Resource):
-
     def get(self, id):
         session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
         session['page_views'] += 1
@@ -52,6 +47,32 @@ api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
+class Login(Resource):
+    def post(self):
+        user = User.query.filter(
+            User.username == request.get_json()['username']
+        ).first()
+        session['user_id'] = user.id
+        return user.to_dict(), 200
+
+api.add_resource(Login, '/login')
+
+class Logout(Resource):
+    def delete(self): 
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+    
+api.add_resource(Logout, '/logout')
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict(), 200
+        else:
+            return {}, 401
+        
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
